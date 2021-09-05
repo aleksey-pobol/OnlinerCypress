b4c0feaf91a4ee61bf772876ecf84b65f005d9ff
Aleksey Pobol <apobal@dealcloud.com>
2021-09-05 14:04:16 +0300

    Updated searchItem

--- cypress/support/pages/mainPage.js
+++ cypress/support/pages/mainPage.js
@@ -9,13 +9,13 @@ export class MainPage{
     }
 
     searchItem(item){
-        cy.get('.fast-search__input').should('be.visible').type(item)        
-        cy.iframe('.modal-iframe').then( iframe => {
-            cy.wrap(iframe).find('.search__input').click()
-            cy.wrap(iframe).find('.product__title-link').each( (listItem) => {
+        cy.get('.fast-search__input').should('be.visible').type(' ')
+        cy.enter('.modal-iframe').then( irfame => {
+            irfame().find('.search__input').type(item) 
+            irfame().find('.product__title-link').each( (listItem) => {
                 const itemText = listItem.text().substr(0, 27)
 
-                cy.wrap(itemText).should('equal', 'Ноутбук Lenovo ThinkPad T14')
+                cy.wrap(itemText).should('equal', 'Ноутбук Lenovo ThinkPad T14')   
             })
         })
     }
