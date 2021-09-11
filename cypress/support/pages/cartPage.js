79e4350b2c04fd68e90cfc87d6179530ecbca108
Aleksey Pobol <apobal@dealcloud.com>
2021-09-12 02:38:18 +0300

    Added assert for removeItemFromCart

--- cypress/support/pages/cartPage.js
+++ cypress/support/pages/cartPage.js
@@ -7,13 +7,16 @@ export class CartPage{
     removeItemFromCart(){        
         // Used {force: true} because trigger ('mouseover') or invoke ('show') don't work
         cy.get('@itemRow').find('.cart-form__button_remove').click({ force: true })
+        cy.get('[class*=cart-form__description_condensed-extra]').then((description) => {
+            expect(description.text().includes('Вы удалили')).to.be.true
+        })
     }
 
     getItemRow(itemName){
         cy.get('.cart-form__offers-item_secondary').then((item) => {
             cy.wrap(item).get('.cart-form__description > a[href]').contains(itemName)
         })
-        .parents('.cart-form__offers-unit_primary').as('itemRow')            
+        .parents('.cart-form__offers-unit_primary').as('itemRow')        
     }
 }
 
